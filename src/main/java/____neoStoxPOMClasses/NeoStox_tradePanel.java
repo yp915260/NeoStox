5d69fe0fd7668899c57df92631ad1de462d9ab68
Yogesh <yp15080@gmail.com>
2023-01-31 11:51:37 -0800

    added trade_panel class

---
+++ src/main/java/____neoStoxPOMClasses/NeoStox_tradePanel.java
@@ -0,0 +1,15 @@
+package ____neoStoxPOMClasses;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NeoStox_tradePanel {
+
+	public static void main(String[] args)
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("");
+
+	}
+
+}
